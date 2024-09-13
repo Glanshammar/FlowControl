4fa533a4deb6be6bf415684cec894d6d39b6ce9d
Andreas Johansson <glanshammar70@gmail.com>
2024-09-13 06:19:20 +0200

    Initial commit

---
+++ FlowControl/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq.Expressions;
+
+namespace FlowControl;
+
+class Program
+{
+    static public void Greeting()
+    {
+        Console.WriteLine("Hello, World!" +
+                          "\nWhat do you want to do?" +
+                          "\n0. Quit" +
+                          "\n1. Enter the movies." +
+                          "\n2. ??");
+    }
+    
+    static void Main(string[] args)
+    {
+        bool running = true;
+        Greeting();
+
+        do
+        {
+            string input = Console.ReadLine()?.Trim().ToLower() ?? string.Empty;
+
+            switch (input)
+            {
+                case "0":
+                    running = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid input. Try again.");
+                    break;
+            }
+        } while (running);
+    }
+}
