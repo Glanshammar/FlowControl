abf5e7707949ccda8aa3cafd0f8175e56108b953
Andreas Johansson <glanshammar70@gmail.com>
2024-09-15 18:44:33 +0200

    Added the TenTimes print function.

---
+++ FlowControl/TenTimes.cs
@@ -0,0 +1,20 @@
+﻿namespace FlowControl;
+
+public class TenTimes
+{
+    public static void Print()
+    {
+        Console.Write("What do you want to print? ");
+        string tenInput = Console.ReadLine()?.Trim() ?? string.Empty;
+        
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.Write($"{i}. {tenInput}");
+            if (i < 10)
+            {
+                Console.Write(", ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
