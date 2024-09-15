da1f491dc043e4ea04b520392343c9f7f8efd95a
Andreas Johansson <glanshammar70@gmail.com>
2024-09-15 18:44:18 +0200

    Added the SplitString function.

---
+++ FlowControl/SplitString.cs
@@ -0,0 +1,23 @@
+﻿namespace FlowControl;
+
+public class Split
+{
+    public static void Input()
+    {
+        Console.WriteLine("\nSplits a sentence and shows the third word.");
+        Console.Write("Write a sentence (at least 3 words): ");
+        
+        string input = Console.ReadLine();
+        string[] words = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+        if (words.Length >= 3)
+        {
+            string thirdWord = words[2];
+            Console.WriteLine($"The third word is: {thirdWord}");
+        }
+        else
+        {
+            Console.WriteLine("The sentence must contain at least 3 words.");
+        }
+    }
+}
